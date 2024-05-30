1d2adc91c314f9626afbd64438df0176701e4e09
Blandine Forntoh Ngwing-allah <bforntohngwingallah@gmail.com>
2024-05-30 14:58:58 +0100

    Initial structure

---
+++ src/main/java/com/example/Pentlux_Restaurant_System/PentluxRestaurantSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pentlux_Restaurant_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentluxRestaurantSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PentluxRestaurantSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pentlux_Restaurant_System/PentluxRestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pentlux_Restaurant_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PentluxRestaurantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
