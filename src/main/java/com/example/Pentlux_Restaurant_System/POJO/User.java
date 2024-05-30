cc19ec253f30087e1c9b9eee8712db64c477aebf
Blandine Forntoh Ngwing-allah <bforntohngwingallah@gmail.com>
2024-05-30 15:35:16 +0100

    Added the model/structure for the user

---
+++ src/main/java/com/example/Pentlux_Restaurant_System/POJO/User.java
@@ -0,0 +1,43 @@
+package com.example.Pentlux_Restaurant_System.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "mobileNumber")
+    private String mobileNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+}
